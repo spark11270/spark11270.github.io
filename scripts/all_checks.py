b980b1bbaca286c004a499d5ca18426aaea97ba8
My name <me@example.com>
2020-07-30 14:53:26 -0400

    Call check_reboot from main, exit with 1 on error

---
+++ scripts/all_checks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+def check_reboot():
+    return os.path.exist("/run/reboot-required")
+
+def main():
+    if check_reboot()
+    print("Pending Reboot.")
+    sys.exit(1)
+
+main()
